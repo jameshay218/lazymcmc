5ac7ea66311b416792274c2ad76c21f7611fc89f
James Hay <jameshay218@gmail.com>
2021-09-14 13:20:10 -0400

    Updated load_mcmc_chains to read in vector of file paths

--- R/analyses.R
+++ R/analyses.R
@@ -70,7 +70,7 @@ calculate_AIC <- function(chain, parTab){
 #' Read in MCMC chains
 #'
 #' Loads all available MCMC chains from the chosen working directory, allowing the user to specify properties of the return chain
-#' @param location the full file path to the MCMC chain containing directory
+#' @param location Either the full file path to the MCMC chain containing directory, or a vector of file paths with the MCMC chainsa
 #' @param parTab the parameter table that was used to solve the model (mainly used to find which were free parameters)
 #' @param unfixed Boolean, if TRUE, only returns free parameters
 #' @param thin thin chain by this much
@@ -83,56 +83,68 @@ calculate_AIC <- function(chain, parTab){
 load_mcmc_chains <- function(location="",parTab,unfixed=TRUE, thin=1,
                              burnin=100000, multi=TRUE, chainNo=FALSE,
                              PTchain=FALSE){
+  if(length(location) == 1 && dir.exists(location)){
+    print("Reading in chains from directory")
     if(multi){
-        chains <- Sys.glob(file.path(location,"*multivariate_chain.csv"))
+      chains <- Sys.glob(file.path(location,"*multivariate_chain.csv"))
     } else {
-        chains <- Sys.glob(file.path(location,"*univariate_chain.csv"))
+      chains <- Sys.glob(file.path(location,"*univariate_chain.csv"))
     }
     if(PTchain){
-        chains <- Sys.glob(file.path(location,"*_chain.csv"))
+      chains <- Sys.glob(file.path(location,"*_chain.csv"))
     }
-    print(chains)
-    if(length(chains) < 1){
-        message("Error - no chains found")
-        return(NULL)
+  } else {
+    print("Reading in chains from filepaths")
+    if(length(location) == 1){
+      chains <- list(location)
+    } else {
+      chains <- as.list(location)
     }
-
-    ## Read in the MCMC chains with fread for speed
-    read_chains <- lapply(chains,data.table::fread,data.table=FALSE)
-
-    ## Thin and remove burn in
-    read_chains <- lapply(read_chains, function(x) x[seq(1,nrow(x),by=thin),])
-    read_chains <- lapply(read_chains,function(x) x[x$sampno > burnin,])
-    print(lapply(read_chains, nrow))
-    
-    if(chainNo){
-        for(i in 1:length(read_chains)) read_chains[[i]]$chain <- i
-    }
-    
-    ## Get the estimated parameters only
-    if(unfixed){
-        fixed <- parTab$fixed
-        read_chains <- lapply(read_chains, function(x) x[,c(which(fixed==0)+1,ncol(x))])
-    }
-
-    ## Try to create an MCMC list. This might not work, which is why we have a try catch
-    list_chains <- tryCatch({
-        tmp_list <- lapply(read_chains,coda::as.mcmc)
-        tmp_list <- coda::as.mcmc.list(tmp_list)
-    }, warning = function(w){
-        print(w)
-        NULL
-    }, error = function(e){
-        print(e)
-        NULL
-    },
-    finally = {
-        tmp_list
-    })
-    
-    chain <- coda::as.mcmc(do.call("rbind",read_chains))
-    return(list("list"=list_chains,"chain"=chain))
+  }
+  
+  print(chains)
+  if(length(chains) < 1){
+    message("Error - no chains found")
+    return(NULL)
+  }
+  
+  ## Read in the MCMC chains with fread for speed
+  read_chains <- lapply(chains,data.table::fread,data.table=FALSE)
+  
+  ## Thin and remove burn in
+  read_chains <- lapply(read_chains, function(x) x[seq(1,nrow(x),by=thin),])
+  read_chains <- lapply(read_chains,function(x) x[x$sampno > burnin,])
+  print(lapply(read_chains, nrow))
+  
+  if(chainNo){
+    for(i in 1:length(read_chains)) read_chains[[i]]$chain <- i
+  }
+  
+  ## Get the estimated parameters only
+  if(unfixed){
+    fixed <- parTab$fixed
+    read_chains <- lapply(read_chains, function(x) x[,c(1,which(fixed==0)+1,ncol(x))])
+  }
+  
+  ## Try to create an MCMC list. This might not work, which is why we have a try catch
+  list_chains <- tryCatch({
+    tmp_list <- lapply(read_chains,coda::as.mcmc)
+    tmp_list <- coda::as.mcmc.list(tmp_list)
+  }, warning = function(w){
+    print(w)
+    NULL
+  }, error = function(e){
+    print(e)
+    NULL
+  },
+  finally = {
+    tmp_list
+  })
+  
+  chain <- coda::as.mcmc(do.call("rbind",read_chains))
+  return(list("list"=list_chains,"chain"=chain))
 }
+
 
 #' Summarise MCMC chain statistics
 #'
